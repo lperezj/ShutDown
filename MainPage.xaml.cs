99238a24044c25179f6d9fe87b5b574892eb7918
Luis Pérez Jiménez <luis.perez@sedecal.com>
2022-07-21 09:42:07 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ShutDown.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShutDown
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            IShutDownService dependency = DependencyService.Get<IShutDownService>();
+            dependency.ShutdownSystem();
+        }
+    }
+}
---
+++ Service/IShutDownService.cs
@@ -0,0 +1,20 @@
+﻿namespace ShutDown.Service
+{
+    public interface IShutDownService
+    {
+        /// <summary>
+        /// https://stackoverflow.com/questions/46452767/how-to-shutdown-android-device-in-c-sharp-xamarin
+        /// </summary>
+        void ShutDown();
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/22763020/how-to-shut-down-android-phone-programmatically
+        /// </summary>
+        void ShutDownProcess();
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/10411650/how-to-shutdown-an-android-mobile-programmatically
+        /// </summary>
+        void ShutdownSystem();
+    }
+}
